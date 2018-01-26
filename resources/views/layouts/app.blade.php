72d63d8aec9258be06547d3d1aca4e43bc611ee4
sho23 <testuser.shota@gmail.com>
2018-01-26 14:55:28 +0900

    フッターの追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,17 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
         <link href="/css/style.css" rel="stylesheet" type="text/css">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
         <div class="container">
             @yield('content')
         </div>
+        <footer class="footer text-center">
+          <div class="container">
+            <p class="text-muted">Created by <a href="https://twitter.com/Shota_Nakai" target="_blank">Sho <i class="fa fa-twitter-square" aria-hidden="true"></i></a></p>
+          </div>
+        </footer>
     </body>
 </html>
